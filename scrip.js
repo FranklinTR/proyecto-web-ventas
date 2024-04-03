853bfdc8271c5f49ef4591079efb06d611aff481
FranklinTR <tfranklin083@gmail.com>
2024-04-02 20:14:20 -0500

    creacion de card sesion, agregacion del login, cambio dinamico de texto de corro en card

--- scrip.js
+++ scrip.js
@@ -16,3 +16,31 @@ productContent.forEach((container) => {
     });
 });
 
+const openCardBtn = document.getElementById("openCardBtn");
+const card = document.getElementById("card");
+
+openCardBtn.addEventListener("click", () => {
+    card.style.display = "block";
+    document.addEventListener("click", closeCardOutside);
+});
+
+function closeCardOutside(event) {
+    if (!card.contains(event.target) && event.target !== openCardBtn) {
+        card.style.display = "none";
+        document.removeEventListener("click", closeCardOutside);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Verificar si hay un correo electrónico en localStorage
+    let userEmail = localStorage.getItem('userEmail');
+    if (userEmail) {
+        // Actualizar el texto del span con el correo almacenado
+        let spanUsuarioCorreo = document.querySelector("#usuarioCorreo");
+        if (spanUsuarioCorreo) {
+            spanUsuarioCorreo.textContent = userEmail;
+        }
+    }
+});
+
+
